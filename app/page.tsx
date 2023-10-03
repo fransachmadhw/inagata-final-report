0fba0ca0b998959c7db7b5a64697dd8bbf2c670a
frans <franswinata6@gmail.com>
2023-10-03 09:23:18 +0700

    Update 1080p

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Head from 'next/head';
 import CustomCursor from './components/CustomCursor';
 import Hero from './components/Hero';
 import ScrollSection from './components/ScrollSection';
@@ -8,6 +9,18 @@ import Greet from './components/Greet';
 export default function Home() {
   return (
     <div className="main-container w-full text-white">
+      <Head>
+        <title>Frans | Inagata Final Report</title>
+        <meta
+          name="description"
+          content="Frans is a front end developer who specializes in building digital products powered with popular technologies."
+        />
+        <link
+          rel="icon"
+          type="image/svg+xml"
+          href="/icons/face.svg"
+        />
+      </Head>
       {/* Mobile */}
       <div className="xl:hidden w-full">
         <Warning />
